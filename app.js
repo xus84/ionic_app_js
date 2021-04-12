b21ac25781976ba13090c6c47ca91e49ec5a7a88
garsan84 <jgrsan84@gmail.com>
2021-04-12 23:39:53 +0200

    button save added

---
+++ app.js
@@ -0,0 +1,21 @@
+const productName = document.querySelector('#productName');
+const productPrice = document.querySelector('#productPrice');
+const buttonSave = document.querySelector('#button-save');
+const buttonCancel = document.querySelector('#button-cancel');
+const productList = document.querySelector('#product-list');
+const totalOutput = document.querySelector('#total');
+
+
+const createNewProduct = (name, price) => {
+    const ionCard = document.createElement('ion-card');
+    const newProductItem = document.createElement('ion-card-content');
+    ionCard.appendChild(newProductItem)
+    newProductItem.textContent = name + ": $ " + price;
+    productList.appendChild(ionCard);
+};
+
+buttonSave.addEventListener("click", () => {
+    const name = productName.value;
+    const price = productPrice.value;
+    createNewProduct(name, price );
+})
